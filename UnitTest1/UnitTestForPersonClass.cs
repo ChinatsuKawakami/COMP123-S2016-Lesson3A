c51be7c3d01630a965211c2112f6162197aaa1cb
ChinatsuKawakami <zarusoba3939@gmail.com>
2016-05-24 13:08:20 -0400

    Added UnitTestFor ProgramClass

---
+++ UnitTest1/UnitTestForPersonClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest1
+{
+    /// <summary>
+    /// Summary description for UnitTestForPersonClass
+    /// </summary>
+    [TestClass]
+    public class UnitTestForPersonClass
+    {
+      
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
---
+++ UnitTest1/UnitTestForProgramClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using COMP123_S2016_Lesson3;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTestForLesson3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
