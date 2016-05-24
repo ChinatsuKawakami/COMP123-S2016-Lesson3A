41b45583d77bc07fb84a93ad5fe8899748cbbd4c
ChinatsuKawakami <zarusoba3939@gmail.com>
2016-05-24 12:56:27 -0400

    Added Person class

---
+++ COMP123-S2016-Lesson3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*This is driver class(program.cs file)
+ * Author:Chinatsu Kawakami
+ * 
+ * 
+ */
+namespace COMP123_S2016_Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
